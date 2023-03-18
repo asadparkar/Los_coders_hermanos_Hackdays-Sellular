c622a4ee3cb67da21018847a333ff6c85f585952
asadparkar <58441390+asadparkar@users.noreply.github.com>
2023-03-19 02:09:14 +0530

    feat: Added Several API's for Users

--- backend/routers/UserRoutes.js
+++ backend/routers/UserRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const UserControllers = require('../controllers/UserControllers')
+const auth = require('../middlewares/Auth')
+
 
 router.post('/signup', UserControllers.Signup);
+router.post('/login', UserControllers.Login);
+router.post('/update',auth,UserControllers.UpdateProfile);
+router.get('/profile',auth,UserControllers.ViewProfile);
+router.get('/myprofile/:id',auth,UserControllers.ViewMyProfile)
+router.get('/users',auth,UserControllers.exploreUsers)
+
+
+
 
 module.exports = router
